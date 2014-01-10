1aea90503df8e39c4fcc5b7cd55d32751d865f6e
Carlos Silva <cefs1984@gmail.com>
2014-01-10 12:46:03 -0300

    adicionando metodo personalizado no modelo.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +11,9 @@ class Poll(models.Model):
 	def __unicode__(self):
 		return self.question
 
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+			
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice_text = models.CharField(max_length=200)
