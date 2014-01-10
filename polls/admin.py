6927fa4f64ef16fc0515e8c827acca372de26f81
Carlos Silva <cefs1984@gmail.com>
2014-01-10 12:56:06 -0300

    Reorganizando campos do formulário admin/poll com fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,9 @@ from polls.models import Poll
 # Register your models here.
 
 class PollAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question']
+	fieldsets = [
+		(None, {'fields': ['question']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+	]
 
 admin.site.register(Poll, PollAdmin)
