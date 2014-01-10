224bddc180ee3bbce2c2e3a90ac915b10937bfa8
Carlos Silva <cefs1984@gmail.com>
2014-01-10 16:01:39 -0300

    Codificando design da página na view.

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import HttpResponse
-
+from polls.models import Poll
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the poll index.")
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]	
+	output = ', '.join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
 
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll %s." % poll_id)
